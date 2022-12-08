f85ea575635283803d34864103672842aa523481
VIVIAN-WANJIRU <viviankariuki028@gmail.com>
2022-12-08 03:35:32 -0800

    hello world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+/* Prints string */
+
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - contains main program code
+ * Return: print string; 0 otherwise
+ */
+
+
+int main(void)
+{
+	char str1[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(str1);
+
+	return (0);
+}
